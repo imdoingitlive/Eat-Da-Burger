da48967cddcb89888dfebaa411901e8ccb34f2e6
Edwin Rivera <edwin.rivera@outlook.com>
2016-08-07 17:18:26 -0400

    added controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+
+var router = express.Router();
+var burger = require('../models/burger.js');
+
+
+
+module.exports = router;
